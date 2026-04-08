12231d4476145e53b7edb121c75fbce92ec70948
Bovoxo <tomas.bovat@gmail.com>
2026-04-08 10:11:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,14 +14,25 @@ app.include_router(random_number.router)
 app.include_router(team_splitter.router)
 app.include_router(lyrics_to_ppt.router)
 app.include_router(youtube_downloader.router)  # <--- NOVÉ ZAPOJENÍ
+
+# TADY JE TEN NOVÝ SEZNAM ADRES:
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://81.2.236.30",
+    "https://yleventdeck.cloud",
+    "https://www.yleventdeck.cloud"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,  # <--- Tady jsme nahradili tu hvězdičku
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
     expose_headers=["Content-Disposition"],
 )
+
 # Můžeme si tu nechat i jednoduchou úvodní stránku, abychom viděli, že server běží
 @app.get("/")
 def read_root():
